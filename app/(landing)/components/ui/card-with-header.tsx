9a057b41a0b7dde93b8886c3b14beb702f53e076
setyadigilangp-dev <setyadigilangp239@gmail.com>
2026-01-18 07:24:00 +0700

    session2

---
+++ app/(landing)/components/ui/card-with-header.tsx
@@ -0,0 +1,18 @@
+type CardWithHeaderProps = {
+    title: string;
+    children: React.ReactNode;
+};
+
+const CardWithHeader = ({title, children}: CardWithHeaderProps) => {
+    return (
+        <div className="bg-white">
+            <div className="px-5 py-4 border-b border-gray-200">
+                <h2 className="font-bold text-lg">{title}</h2>
+            </div>
+            {children}
+        </div>
+    )
+    
+};
+
+export default CardWithHeader;
